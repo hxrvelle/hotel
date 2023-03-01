957da0bd251e8ea1e9cca035c27d65ba878fdc50
hxrvelle <122966458+hxrvelle@users.noreply.github.com>
2023-03-01 20:07:05 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+package hotel;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Hotel hotel = new Hotel();
+        Scanner scanner = new Scanner(System.in);
+        String command;
+        while (true){
+            System.out.println("Введите команду: ");
+            command = scanner.nextLine();
+            if (command.equals("/getFreeRooms")) {
+                hotel.getFreeRooms();
+            } else if (command.equals("/reserve")) {
+                hotel.reserve();
+            } else if (command.equals("/getReservedRooms")) {
+                hotel.getReservedRooms();
+            }
+        }
+    }
+}
+
+/*
+ДЗ:
+скачать git
+ */
